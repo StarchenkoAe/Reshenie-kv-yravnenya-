4f47715f407608f8722f8caf364896d041192748
StarchenkovAE <lacherov@gmail.com>
2024-03-10 13:28:53 +0400

    Пояснения к коду

--- reshenie.py
+++ reshenie.py
@@ -1,11 +1,12 @@
-import math
-print("x^2+6x+9=0")
+import math #Библиотека math для работы с sqrt
+print("x^2+6x+9=0") #Пишем нужное уравнение и вводим коэффиценты
 a = 1.0
 b = 6.0
 c = 9.0
 Discriminant = b ** 2 - 4 * a * c 
 print("Дискриминант D=", Discriminant)
-if Discriminant < 0:
+#Условия D<0 нет решения, D==0 один корень x, D>0 два корня x1, x2
+if Discriminant < 0: 
     print("Нет действительных корней")
 elif Discriminant == 0:
     x = (-b) / (2*a)
